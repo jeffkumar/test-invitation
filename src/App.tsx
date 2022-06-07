7c5e830dd81c75f8dc2782180ee80da75c0a53a9
Jeff <jeff@Jeffs-MacBook-Pro.local>
2022-06-07 11:56:27 -0400

    some fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,111 +1,39 @@
-import React,  { useEffect, useState, useRef }  from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { postData } from "./service/InviteService"; 
-
-const validateEmail = (email) => {
-    return String(email)
-    .toLowerCase()
-    .match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
-};
+import React,  { useState }  from 'react';
+import Form  from "./Form"; 
+import './App.css'; 
  
 export default function App() {
 
-  const name = useRef(null); 
-  const email = useRef(null); 
-  const guests = useRef(null); 
-  const coming = useRef(null); 
-  const [showThanks, setShowThanks] = useState(false); 
-  const [error, setError] = useState(""); 
-  const onSubmit = (e: any) => {
-    e.preventDefault();  
-    setError(""); 
-    if (name.current.value == "") {
-        setError("Please enter your name"); 
-        return;
-    }   
-    if (!validateEmail(email.current.value)) {
-        setError("Please enter a valid email"); 
-        return;
+    const [formSubmitted, setFormSubmitted] = useState(false); 
+
+    if (formSubmitted) {
+        return (
+            <main>
+                <h1>RSVP</h1>
+                <p>Thank you for your response</p>
+                <p> 
+                Please check out <a href="https://www.lakehill.pl/en/"> Lake Hill Resort and Spa</a>
+                </p>
+            </main>
+        ); 
     }
-    if (Number.isInteger(parseInt(guests.current.value)) !== true) {
-        setError("Please enter a valid number of guests"); 
-        return;
-    }    
 
-    postData('https://adayinthelife.app/test-connect.php', { 
-        name: name.current.value, 
-        email: email.current.value,
-        guests: parseInt(guests.current.value),
-        iscoming: !coming.current.checked ? 1 : 0, 
-        comments: ""
-    }).then(
-      (data) => {
-        console.log(data); // JSON data parsed by `data.json()` call
-      }
-    ).finally(()=> {
-        setShowThanks(true); 
-    }); 
-  }; 
-
-  if (showThanks) {
     return (
-    <main>
-        <h3>RSVP</h3>
-        <p>Thank you for your response</p>
-        <p> 
-          Please check out <a href="https://www.lakehill.pl/en/"> Lake Hill Resort and Spa</a>
-        </p>
-    </main>); 
-  }
+        <main> 
+            <h1>RSVP</h1> 
+            <div>
+                <p>
+                Jeff and Anastasiia would like to invite you to join us in Jelenia
+                Gora, Poland on July 24, 2022.
+                </p>
+                <p>
+                If you would like to make a reservation. We advise you to stay at the
+                <a href="https://www.lakehill.pl/en/"> Lake Hill Resort and Spa</a>.
+                </p>
+                <Form setFormSubmitted={setFormSubmitted}>
 
-  return (
-    <main> 
-      <h3>RSVP</h3>
-      
-      <div>
-        <p>
-          Jeff and Anastasiia would like to invite you to join us in Jelenia
-          Gora, Poland on July 24, 2022.
-        </p>
-        <p>
-          If you would like to make a reservation. We advise you to stay at the
-          <a href="https://www.lakehill.pl/en/"> Lake Hill Resort and Spa</a>.
-        </p>
-      </div>
-      <div>
-        {error !== "" && 
-          <div className="form-error">{error}</div>
-        }
-        <form onSubmit={(e) => onSubmit(e)}>
-           
-          <div className="form-control">
-            <label htmlFor="name">Name</label>
-            <input ref={name}  id="name" type="text" placeholder="your name" />
-          </div>
-          <div className="form-control">
-            <label htmlFor="email">Email</label>
-            <input ref={email} id="email" type="email"  placeholder="your email" />
-          </div>
-          <div className="form-control">
-            <label htmlFor="guests">Number of guests coming</label>
-            <input ref={guests} id="guests" name="guests" style={{ width: '32px' }} placeholder=""  />
-          </div>
-          <div className="form-control">
-            <input ref={coming} id="coming" type="radio" name="test" value="true" defaultChecked/>
-            <label htmlFor="coming">Joyfully accept</label>
-          </div>
-          <div>
-            <input ref={coming} id="coming2" type="radio" name="test" value="false"/>
-            <label htmlFor="coming2">Sadly decline</label>
-          </div>
-          <div className="form-control">
-            <button onClick={(e) => onSubmit(e)}>RSVP</button>
-          </div>
-        </form>
-      </div>
-    </main>
-  );
+                </Form>
+            </div> 
+        </main>
+    );
 }
---
+++ src/Form.tsx
@@ -0,0 +1,83 @@
+import React,  { useEffect, useState, useRef }  from 'react';
+import { postData } from "./service/InviteService"; 
+import { validateEmail } from "./service/utils"; 
+
+export default function Form({ setFormSubmitted }: any) {
+
+  const name = useRef<HTMLInputElement>(null); 
+  const email = useRef<HTMLInputElement>(null); 
+  const guests = useRef<HTMLInputElement>(null); 
+  const coming = useRef<HTMLInputElement>(null); 
+  const comments = useRef<HTMLTextAreaElement>(null); 
+  const [showThanks, setShowThanks] = useState(false); 
+  const [error, setError] = useState(""); 
+  const onSubmit = (e: any) => {
+
+    e.preventDefault();  
+    setError(""); 
+    if (name?.current?.value == "") {
+        setError("Please enter your name"); 
+        return;
+    }   
+    if (!validateEmail(email?.current?.value)) {
+        setError("Please enter a valid email"); 
+        return;
+    }
+    if (Number.isInteger(parseInt(guests?.current?.value || "")) !== true) {
+        setError("Please enter a valid number of guests"); 
+        return;
+    }    
+
+    postData('https://adayinthelife.app/test-connect.php', { 
+        name: name?.current?.value, 
+        email: email?.current?.value,
+        guests: parseInt(guests?.current?.value || "0"),
+        iscoming: !coming?.current?.checked ? 1 : 0, 
+        comments: comments?.current?.value
+    }).then(
+      (data) => {
+        console.log(data); // JSON data parsed by `data.json()` call
+      }
+    ).finally(()=> {
+        setFormSubmitted(true); 
+    }); 
+  }; 
+
+  return (
+    <div>
+        {error !== "" && 
+        <div className="form-error">{error}</div>
+        }
+        <form onSubmit={(e) => onSubmit(e)}>  
+            <div className="form-control">
+                <label htmlFor="name">Name</label>
+                <input ref={name}  id="name" type="text" placeholder="your name" />
+            </div>
+            <div className="form-control">
+                <label htmlFor="email">Email</label>
+                <input ref={email} id="email" type="email"  placeholder="your email" />
+            </div>
+            <div className="form-control">
+                <label htmlFor="guests">Number of guests coming</label>
+                <input ref={guests} id="guests" name="guests" style={{ width: '32px' }} placeholder=""  />
+            </div>
+            <div className="form-control">
+                <input ref={coming} id="coming" type="radio" name="test" value="true" defaultChecked/>
+                <label htmlFor="coming" className="radio-label">Joyfully accept</label>
+            </div>
+            <div className="form-control">
+                <input ref={coming} id="coming2" type="radio" name="test" value="false"/>
+                <label htmlFor="coming2" className="radio-label">Sadly decline</label>
+            </div>
+            <div className="form-control">
+                <label htmlFor="comments">Leave a note</label>
+                <br />
+                <textarea id="comments" ref={comments} name="comments" /> 
+            </div>
+            <div className="form-control">
+                <button onClick={(e) => onSubmit(e)}>RSVP</button>
+            </div>
+        </form>
+    </div>
+  );
+}; 
