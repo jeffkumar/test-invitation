af3c0e239a048480d66ceb5bae5db2adc00bdee2
Jeff <jeff@Jeffs-MacBook-Pro.local>
2022-06-07 22:27:41 -0400

    adding guest list page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React,  { useState }  from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Intro  from "./Intro"; 
 import ThankYou  from "./ThankYou"; 
+import GuestList  from "./GuestList"; 
 import './App.css';  
  
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
             <Routes>
                 <Route path="/" element={<Intro />} />
                 <Route path="/thankyou" element={<ThankYou />} />
+                <Route path="/guestlist" element={<GuestList />} />
             </Routes>
         </BrowserRouter> 
     );
---
+++ src/GuestList.tsx
@@ -0,0 +1,46 @@
+ 
+
+import React, { useState, useEffect }  from 'react'; 
+ 
+export default function GuestList() {
+  
+    const [list, setList] = useState<any>([]); 
+
+    useEffect(() => {
+        fetch('https://adayinthelife.app/list.php')
+            .then(data => data.json())
+            .then((data) => {
+                setList(data); 
+                console.log(data); 
+            }
+        );
+    })
+    
+
+  return (
+    <main> 
+        <h1>Guest List</h1>
+        <section> 
+            <header>
+                <div className="col">Name</div>
+                <div className="col">Email</div>
+                <div className="col half">Guests</div>
+                <div className="col half">Coming</div>
+                <div className="col">Comments</div>
+            </header>
+            {list.map((item: any) => {
+                return (
+                    <div className="row"> 
+                        <div className="col">{item.name}</div>
+                        <div className="col">{item.email}</div>
+                        <div className="col half">{item.guests}</div>
+                        <div className="col half">{item.iscoming ? "yes" : "no"}</div>
+                        <div className="col">{item.comments}</div>
+                    </div>
+                );
+            })}
+        </section>
+         
+    </main>
+  );
+}; 
