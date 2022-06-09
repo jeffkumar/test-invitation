5efedb65ee87fc3320b58bf10875013a4342b875
Jeff <jeff@Jeffs-MacBook-Pro.local>
2022-06-09 01:26:59 -0400

    fixing flex design

--- src/Intro.tsx
+++ src/Intro.tsx
@@ -4,16 +4,17 @@ import { Clocks } from "./Clock";
 export default function Intro() {
     return (
         <main>
-            <h1>RSVP</h1> 
-            
-            <div>
-                <p>
-                    Jeff and Anastasiia would like to invite you to join us in Jelenia
-                    Gora, Poland on July 24, 2022.
-                </p>  
-                <Clocks />
-                <Form /> 
+            <h1>RSVP</h1>  
+            <div className="intro-section">
+                <div>
+                    <p>
+                        Jeff and Anastasiia would like to invite you to join us in Jelenia
+                        Gora, Poland on July 24, 2022.
+                    </p>  
+                </div> 
+                <Clocks /> 
             </div> 
+            <Form /> 
         </main> 
     );
 };
